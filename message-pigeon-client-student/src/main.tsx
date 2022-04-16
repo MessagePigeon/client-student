7bb28011890a113c4f0f565fa0df97a3bf06148b
Justin Sun <i@jsun969.cn>
2022-04-17 00:27:25 +0800

    feat: add antd and tailwind css

--- message-pigeon-client-student/src/main.tsx
+++ message-pigeon-client-student/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import 'antd/dist/antd.css';
+import './tailwind.css';
 
 ReactDOM.render(
   <React.StrictMode>
