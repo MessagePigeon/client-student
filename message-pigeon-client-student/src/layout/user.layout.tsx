3792499691cccb3b2d19c732b5a4544459094b1b
Justin Sun <i@jsun969.cn>
2022-04-18 20:52:42 +0800

    feat: add basic user layout

--- message-pigeon-client-student/src/layout/user.layout.tsx
+++ message-pigeon-client-student/src/layout/user.layout.tsx
@@ -1,12 +1,55 @@
+import { useRequest } from 'ahooks';
+import { Button, Tabs, Typography } from 'antd';
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { API } from '../http/apis';
 
 const UserLayout: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { data } = useRequest(API.init, {
+    onError() {
+      localStorage.removeItem('token');
+      navigate('/login');
+    },
+  });
+
   return (
-    <>
-      <div>UserLayout</div>
-      <Outlet />
-    </>
+    <div className="container mx-auto pt-2">
+      <Tabs
+        defaultActiveKey={location.pathname}
+        onTabClick={(key) => navigate(key)}
+        centered
+        tabBarExtraContent={{
+          left: (
+            <Typography.Text className="text-xl">
+              {data?.data.defaultRemark}
+            </Typography.Text>
+          ),
+          right: (
+            <Button
+              onClick={() => {
+                localStorage.removeItem('token');
+                navigate('/login');
+              }}
+            >
+              退出登录
+            </Button>
+          ),
+        }}
+      >
+        <Tabs.TabPane tab="历史记录" key="/history">
+          {location.pathname === '/history' && <Outlet />}
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="连接码" key="/connect-code">
+          {location.pathname === '/connect-code' && <Outlet />}
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="已绑定教师" key="/teacher">
+          {location.pathname === '/teacher' && <Outlet />}
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
   );
 };
 
