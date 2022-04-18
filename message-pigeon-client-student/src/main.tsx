dc9a54f1d3a23f299fecffdeed15b533ffafd30a
Justin Sun <i@jsun969.cn>
2022-04-18 09:05:04 +0800

    feat: remove react strict mode

--- message-pigeon-client-student/src/main.tsx
+++ message-pigeon-client-student/src/main.tsx
@@ -1,15 +1,12 @@
 import 'antd/dist/antd.css';
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app';
 import './tailwind.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
