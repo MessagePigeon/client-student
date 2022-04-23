12428aeb0395c4ed04afad073e844afe6fde3eac
Justin Sun <i@jsun969.cn>
2022-04-23 19:56:14 +0800

    feat: remove author

--- message-pigeon-client-student/src/layout/base.layout.tsx
+++ message-pigeon-client-student/src/layout/base.layout.tsx
@@ -10,7 +10,7 @@ const BaseLayout: React.FC = () => {
       </main>
       <footer className="fixed inset-x-0 bottom-0 py-2 bg-white">
         <Typography.Text type="secondary" className="block text-center">
-          作者: 荆棘Justin Github:{' '}
+          Github开源地址:{' '}
           <Typography.Link
             href="https://github.com/MessagePigeon/client-student"
             target="_blank"
