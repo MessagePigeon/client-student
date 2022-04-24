8c8b6464b6b5e973ec5b28c4a909ededf559d595
Justin Sun <i@jsun969.cn>
2022-04-24 18:24:39 +0800

    feat: add refresh button in connect code card

--- src/connect-code/connect-code.page.tsx
+++ src/connect-code/connect-code.page.tsx
@@ -1,4 +1,4 @@
-import { Card, Typography } from 'antd';
+import { Button, Card, Typography } from 'antd';
 import { QRCodeSVG } from 'qrcode.react';
 import React from 'react';
 import { useAppSelector } from '../state/hooks';
@@ -15,7 +15,15 @@ const ConnectCodePage: React.FC = () => {
 
   return (
     <>
-      <Card title="连接代码" className="mb-3">
+      <Card
+        title="连接代码"
+        className="mb-3"
+        extra={
+          <Button type="link" size="small" onClick={() => location.reload()}>
+            刷新
+          </Button>
+        }
+      >
         <Typography.Text className="text-3xl">{connectCode}</Typography.Text>
       </Card>
       <Card title="二维码">
