c8269aefbb01a8105cab08fa0c8b14eb63dca95d
Justin Sun <i@jsun969.cn>
2022-04-24 21:27:19 +0800

    feat: add system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,50 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{
+  CustomMenuItem, Manager, RunEvent, SystemTray, SystemTrayEvent, SystemTrayMenu, WindowEvent,
+};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(CustomMenuItem::new("show".to_string(), "显示主界面"))
+    .add_item(CustomMenuItem::new("quit".to_string(), "退出"));
+  let system_tray = SystemTray::new().with_menu(tray_menu);
+
+  let app = tauri::Builder::default()
+    .system_tray(system_tray)
+    .on_system_tray_event(|app_handle, e| match e {
+      SystemTrayEvent::LeftClick { .. } => {
+        let window = app_handle.get_window("main").unwrap();
+        window.show().unwrap();
+        window.set_focus().unwrap();
+      }
+      SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+        "show" => {
+          let window = app_handle.get_window("main").unwrap();
+          window.show().unwrap();
+        }
+        "quit" => {
+          std::process::exit(0);
+        }
+        _ => {}
+      },
+      _ => {}
+    })
+    .build(tauri::generate_context!())
     .expect("error while running tauri application");
+
+  app.run(|app_handle, e| match e {
+    RunEvent::WindowEvent {
+      label,
+      event: WindowEvent::CloseRequested { api, .. },
+      ..
+    } => {
+      let app_handle = app_handle.clone();
+      let window = app_handle.get_window(&label).unwrap();
+      api.prevent_close();
+      window.hide().unwrap();
+    }
+    _ => {}
+  })
 }
